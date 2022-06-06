157ed60ebeb2efe4b2bd1cab451aae828e10c77b
cyberjam <jamsince1994@naver.com>
2022-06-06 13:50:44 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./egoingTest/index.css";
+import AppApp from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("app_root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppApp />
   </React.StrictMode>
 );
 
